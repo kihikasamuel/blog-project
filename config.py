02bccc5b043f91a79d5c4b41ce3a59381a5438c9
Summitm <summitros@gmail.com>
2019-02-26 19:03:16 +0300

    H

---
+++ config.py
@@ -0,0 +1,33 @@
+"""Def diff application envs"""
+
+class Config(object):
+	"""
+	Common config to all envs
+	"""
+
+	DEBUG = True
+
+class DevelopmentConfig(Config):
+	"""
+	Configs for development env
+	"""
+	SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+	"""
+	Configs for production env
+	"""
+	DEBUG = False
+
+class TestingConfig(Config):
+	"""
+	Configs for Test env
+	"""
+	TESTING = True
+
+
+app_config = {
+	'development': DevelopmentConfig,
+	'production': ProductionConfig,
+	'testing': TestingConfig
+}
