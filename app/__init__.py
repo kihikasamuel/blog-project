00290f6cb497e43c240ee8f1feb094e7f8222afe
Summitm <summitros@gmail.com>
2019-02-26 19:28:41 +0300

    m

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 # third party imports
+import os
 from flask 				import Flask, render_template
 from flask_bootstrap	import Bootstrap
 from flask_sqlalchemy 	import SQLAlchemy
@@ -13,9 +14,17 @@ login_manager = LoginManager()
 
 # create app
 def create_app(config_name):
-	app = Flask(__name__, instance_relative_config=True)
-	app.config.from_object(app_config[config_name])
-	app.config.from_pyfile('config.py')
+
+	if os.getenv('FLASK_CONFIG') == "production":
+		app = FLASK(__name__)
+		app.config.update(
+				SECRET_KEY=os.getenv('SECRET_KEY'),
+				SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+			)
+	else:
+		app = Flask(__name__, instance_relative_config=True)
+		app.config.from_object(app_config[config_name])
+		app.config.from_pyfile('config.py')
 
 
 	Bootstrap(app)
