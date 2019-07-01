96c03c2c1f22b012a6a4714a5c6ac6855c96b15b
Summitm <summitros@gmail.com>
2019-07-01 20:09:46 +0300

    Hello my caller

--- app/home/views.py
+++ app/home/views.py
@@ -1,14 +1,39 @@
+from __future__ import print_function
 from flask import abort, render_template, jsonify
 from flask_login import login_required, current_user
 
+import africastalking
+
 from . import home
 
+# ============================================================
 @home.route('/')
-def homepage():
-	"""
-	template to be rendered
-	"""
-	return render_template('home/index.html', title='Welcome')
+def call():
+	username = "sandbox"
+	api_key = ""
+
+	africastalking.initialize(username,api_key)
+	voice = africastalking.Voice
+
+	call_from = "+254719166938"
+	call_to = ["+254710701117"]
+
+	try:
+		# make call
+		voice.call(call_from,call_to)
+		print(result)
+	except Exception as e:
+		print ("Encountered an error while making the call:%s" %str(e))
+
+
+
+# =============================================================
+# @home.route('/')
+# def homepage():
+# 	"""
+# 	template to be rendered
+# 	"""
+# 	return render_template('home/index.html', title='Welcome')
 
 @home.route('/dashboard')
 @login_required
